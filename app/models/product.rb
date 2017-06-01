1f0dc6f35d9e12eb64e5975defa96dbc71876baa
yaoyao <yao_zhou@icloud.com>
2017-06-01 01:02:44 -0700

    add category column to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  CATEGORY = ["Tees", "Shirts", "Dresses", "Accessories"]
+  validates_inclusion_of :category, :in => CATEGORY
 end
