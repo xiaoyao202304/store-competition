25484690bc0d9a8042d7c5614f2ab34bc80ffb29
yaoyao <yao_zhou@icloud.com>
2017-06-03 04:44:13 -0700

    implement multi-photo to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+  has_many :photos, :dependent => :destroy
+  accepts_nested_attributes_for :photos
 
   CATEGORY = ["Tees", "Shirts", "Dresses", "Accessories"]
   validates_inclusion_of :category, :in => CATEGORY
